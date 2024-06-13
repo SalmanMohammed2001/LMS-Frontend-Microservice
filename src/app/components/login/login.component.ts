c53e855c135ae6e4bc47ca615ea942b06df3cc31
salman <salmannisthar123@gmail.com>
2024-06-13 19:52:25 +0530

    implementation login and signup implementation

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import {MatFormField, MatFormFieldModule} from "@angular/material/form-field";
+import {MatInput, MatInputModule} from "@angular/material/input";
+import {FormsModule} from "@angular/forms";
+import {MatButtonModule} from "@angular/material/button";
+import {MatIconModule} from "@angular/material/icon";
+import {RouterLink} from "@angular/router";
+
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    MatFormFieldModule, MatInputModule, FormsModule, MatButtonModule, MatIconModule, RouterLink
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
 
+
 }
+
+
+
--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,9 +1,21 @@
 import { Component } from '@angular/core';
+import {FormsModule} from "@angular/forms";
+import {MatButton} from "@angular/material/button";
+import {MatFormField, MatLabel} from "@angular/material/form-field";
+import {MatInput} from "@angular/material/input";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    MatButton,
+    MatFormField,
+    MatInput,
+    MatLabel,
+    RouterLink
+  ],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.scss'
 })
