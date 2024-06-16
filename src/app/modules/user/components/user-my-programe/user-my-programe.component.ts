a6513d43b06457157bfedb90ea53b605a63f8d4d
salman <salmannisthar123@gmail.com>
2024-06-16 11:10:37 +0530

    my programe ul implementation

--- src/app/modules/user/components/user-my-programe/user-my-programe.component.ts
+++ src/app/modules/user/components/user-my-programe/user-my-programe.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import {DatePipe} from "@angular/common";
 
 @Component({
   selector: 'app-user-my-programe',
   standalone: true,
-  imports: [],
+  imports: [
+    DatePipe
+  ],
   templateUrl: './user-my-programe.component.html',
   styleUrl: './user-my-programe.component.scss'
 })
 export class UserMyProgrameComponent {
 
+  date=new Date()
 }
